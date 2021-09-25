f19a571032f0d980f32a9119e0d6a90db23b3e85
Peter Swanton <peterswanton19@outlook.com>
2021-09-25 12:36:39 -0400

    initial setup

---
+++ main.js
@@ -0,0 +1,30 @@
+var util = require('util');
+var fs = require('graceful-fs');
+var deepmerge = require('deepmerge');
+var WordPOS = require('wordpos');
+var wordpos = new WordPOS();
+var natural = require('natural');
+var sentenceTokenizer = new natural.SentenceTokenizer();
+var presentVerbInflector = new natural.PresentVerbInflector();
+var wordnet = new natural.WordNet();
+var sentencer = require('sentencer');
+var txtgen = require('txtgen');
+var writegood = require('write-good');
+var NlpManager = require('node-nlp');
+var nlpManager = new NlpManager({ languages: ['en'], forceNER: true });
+var brain = require('brain.js');
+
+function wordFreq(string) {
+    var words = string.replace(/[.]/g, '').split(/\s/);
+    var freqMap = {};
+    words.forEach(function(w) {
+        if (!freqMap[w]) {
+            freqMap[w] = 0;
+        }
+        freqMap[w] += 1;
+    });
+
+    return freqMap;
+};
+
+module.exports = {util, fs, deepmerge, wordpos, natural, sentenceTokenizer, presentVerbInflector, wordnet, sentencer, txtgen, writegood, nlpManager, brain, wordFreq};
---
+++ test/main.js
@@ -0,0 +1,2 @@
+var cyclopean = require('Cyclopean');
+var callOfCthulhu = require('./call_of_cthulhu.js');
